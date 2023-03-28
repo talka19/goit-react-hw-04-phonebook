1fc1fbd0dc77c4a4cee3e970b3faa4c1806077fd
talka19 <talka19.10@gmail.com>
2023-03-28 12:19:32 +0300

    fixed change for App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,16 @@ import {ContactForm} from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import css from "./App.module.css"
 
+const initPhoneBook = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export default function App () {
 
-  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ?? [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]);
+  const [contacts, setContacts] = useState(() => JSON.parse(window.localStorage.getItem('contacts')) ?? initPhoneBook);
 
   const [filter, setFilter] = useState('');
   
